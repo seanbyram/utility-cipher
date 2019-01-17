1ae70d4f33eff5b34896e02b4636224f2b2a949f
Justin Haupt and Sean Byram <sean@>
2019-01-17 11:22:36 -0800

    add text fields for cipher inputand result. add cipher dropdown and submit button. add caesarian parameter

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,20 @@
 
 $(document).ready(function() {
 
+  $(".btn:first-child").text($(".dropdown-item:first-child").text());
+  $(".btn:first-child").val($(".dropdown-item:first-child").text());
 
+  $("#param1").text("N:");
+  for(let i=0; i<26; ++i) {
+    $("#drop").append("<option value='" + i.toString() + "'>" + i.toString() + "</option>");
+  }
+
+  $(".dropdown-menu a").click(function(){
+
+        $(".btn:first-child").text($(this).text());
+        $(".btn:first-child").val($(this).text());
+
+  });
 
 
 
