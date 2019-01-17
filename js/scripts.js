55907bc222addbbb83d176c5bb16cf84b1e1e8de
Justin Haupt and Sean Byram <justin@>
2019-01-17 14:47:01 -0800

    complete caesar cipher

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,38 @@
 // Business Logic
 
 
+/**
+ * Caeser Encoding. Shift all alphabetic characters to the right in
+ * the alphabet. Non-alphabetic characters remain.
+ */
+function caesar (n, input) {
+
+  var text = input.split("");   // string to array
+
+  for(let i=0; i<text.length; ++i) {
+    let char = parseInt(text[i].charCodeAt());    // convert to ascii value
+    n = parseInt(n);
+    let final = char;           //assume special char
+
+    if(char>96 && char<123) {   //lowercase
+      let x = char + n;         // increment character by N
+        if (x>122) {            // if value is past the last letter
+          x = (x-123) + 97;
+        }
+      final = x;
+    }
+    else if (char>64 && char<91){    //uppercase
+      let x = char + n;         // increment character by N
+        if (x>90) {             // if value is past the last letter
+          x = (x-91) + 65;
+        }
+      final = x;
+    }
+    text[i] = String.fromCharCode(final);       // convert ascii back to string
+  }
+
+  return text.join("");                   // join all characters back together
+};
 
 
 
@@ -41,10 +73,10 @@ $(document).ready(function() {
       case "Caesarian Shift":
         var n = $("#drop").val();
         output = caesar (n, input);
-        $("#output").val();
+        $("#output").val(output);
         break;
 
-    }
+    };
 
 
 
